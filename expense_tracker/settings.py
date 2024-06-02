9a88a6e1a174cbcbf39051d5c76c1208a38a1ad4
Tetiana Kozakova <kleotan901@gmail.com>
2024-06-02 12:42:38 +0300

    Added styles.css

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -124,6 +124,14 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
