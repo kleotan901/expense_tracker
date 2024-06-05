d5801cbab2571ac3366b0f6a825d3be2a5e5d860
Tetiana Kozakova <kleotan901@gmail.com>
2024-06-05 16:39:50 +0300

    Added model Currency and created migrations

--- account/models.py
+++ account/models.py
@@ -1,14 +1,24 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from account.currency_list import CURRENCY_CHOICES
+
 
 class UserAccount(AbstractUser):
     pass
 
 
+class Currency(models.Model):
+    main_currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES)
+
+    def __str__(self):
+        return self.main_currency
+
+
 class Account(models.Model):
     account_type = models.CharField(max_length=255)
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES)
 
     def __str__(self):
-        return f"{self.account_type}: {self.balance}"
+        return f"{self.account_type}: {self.balance} {self.currency}"
