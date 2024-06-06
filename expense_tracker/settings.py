3bfb869578bb374abad7095250e26f303c1a0c25
Tetiana Kozakova <kleotan901@gmail.com>
2024-06-06 09:30:35 +0300

    Added .env.sample

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--h@k@l!hbylshv%(=m43olsyc*=h$y^65mv)7&#i7ex@q&7o11"
+SECRET_KEY = os.environ["SECRET_KEY"]
+CURRENCY_EXCHANGE_API_KEY = os.environ["CURRENCY_EXCHANGE_API_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -137,3 +143,4 @@ INTERNAL_IPS = [
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
