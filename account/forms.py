f3353f867a2d636a1e2aa35360adbd338dce2125
Tetiana Kozakova <kleotan901@gmail.com>
2024-07-19 08:08:09 +0300

    Implemented updating converted_balance and conversion_rate fields the main currency is changed

--- account/forms.py
+++ account/forms.py
@@ -10,13 +10,32 @@ from account.utility import get_exchange_rate
 class UserAccountCreateForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = UserAccount
-        fields = UserCreationForm.Meta.fields + ("first_name", "last_name", "currency_id")
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "currency_id",
+        )
 
 
 class UserCurrencyUpdateForm(forms.ModelForm):
     class Meta:
         model = UserAccount
-        fields = ['currency_id']
+        fields = ["currency_id"]
+
+    def save(self, commit=True):
+        user = super(UserCurrencyUpdateForm, self).save(commit=False)
+        for account in user.accounts.all():
+            account.conversion_rate = get_exchange_rate(
+                account.currency, user.currency_id.code
+            )
+            account.converted_balance = Decimal(account.balance) * Decimal(
+                account.conversion_rate
+            )
+            if commit:
+                account.save()
+        if commit:
+            user.save()
+        return user
 
 
 class AccountForm(forms.ModelForm):
@@ -30,7 +49,9 @@ class AccountForm(forms.ModelForm):
         account = super().save(commit=False)
         base_currency = account.user.currency_id.code
         account.conversion_rate = get_exchange_rate(account.currency, base_currency)
-        account.converted_balance = Decimal(account.balance) * Decimal(account.conversion_rate)
+        account.converted_balance = Decimal(account.balance) * Decimal(
+            account.conversion_rate
+        )
         if commit:
             account.save()
         return account
